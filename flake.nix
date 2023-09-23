30e7cf1f10ff886ef246c40b5ad2f2de29ab8a3b
Kenneth Fang <fangkw42@gmail.com>
2023-09-23 19:10:27 -0400

    Initialize project

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "OCaml Backend Experiment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs {
+        inherit system;
+        config = { allowUnfree = true; };
+      };
+      in {
+        devShell = with pkgs; 
+          let vscodeWithExtensions = 
+            vscode-with-extensions.override {
+              vscodeExtensions = with vscode-extensions; [
+                bbenoist.nix
+                vscodevim.vim
+                ocamllabs.ocaml-platform
+              ];
+            };
+          in 
+            mkShell { 
+              buildInputs = [ 
+                opam
+                vscodeWithExtensions
+                libev
+                openssl
+                pkg-config
+              ]; 
+            };
+      });
+}
