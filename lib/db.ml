ee827bbbd868fa08069d3c655d0ef1a1aaec15df
Kenneth Fang <fangkw42@gmail.com>
2023-10-29 21:04:13 -0400

    Get database working with caqti

--- lib/db.ml
+++ lib/db.ml
@@ -1,1 +1,45 @@
-let url = "postgresql://user:password@postgres/dream"
+let url = "postgresql://ocaml:password@localhost"
+
+(* This is the connection pool we will use for executing DB operations. *)
+let connect () =
+  Caqti_lwt.connect (Uri.of_string url)
+
+(** For `utop` interactions interactions. See `README.md`.
+ *)
+let connect_exn () =
+  let conn_promise = connect () in
+  match Lwt_main.run conn_promise with
+  | Error err ->
+      let msg =
+        Printf.sprintf "Abort! We could not get a connection. (err=%s)\n"
+          (Caqti_error.show err)
+      in
+      failwith msg
+  | Ok module_ -> module_
+
+module Q = struct
+  open Caqti_request.Infix
+
+  (*
+    Caqti infix operators
+
+    ->! decodes a single row
+    ->? decodes zero or one row
+    ->* decodes many rows
+    ->. expects no row
+  *)
+
+  let get_all_points =
+    Caqti_type.(unit ->* tup2 float float)
+    "SELECT x, y FROM app.point"
+
+  let insert_point =
+    Caqti_type.(tup2 float float ->. unit)
+    "INSERT INTO app.point(x, y) VALUES(?, ?)"
+end
+
+let get_points (module Conn : Caqti_lwt.CONNECTION) =
+  Conn.fold Q.get_all_points (fun (x, y) acc -> (Point.create x y)::acc) () []
+
+let insert_point (module Conn : Caqti_lwt.CONNECTION) =
+  Conn.exec Q.insert_point
--- lib/db.mli
+++ lib/db.mli
@@ -1,1 +1,20 @@
 val url : string
+
+val connect :
+  unit ->
+  ( Caqti_lwt.connection,
+    [> Caqti_error.load_or_connect ] )
+  result
+  Lwt.t
+
+val connect_exn : unit -> Caqti_lwt.connection
+
+val get_points :
+  (module Caqti_lwt.CONNECTION) ->
+  (Point.point list, [> Caqti_error.call_or_retrieve ]) result
+  Lwt.t
+
+val insert_point :
+  (module Caqti_lwt.CONNECTION) ->
+  float * float ->
+  (unit, [> Caqti_error.call_or_retrieve ]) result Lwt.t
